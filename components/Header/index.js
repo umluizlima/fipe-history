07e63985b31ca014d4076d637e586708b2ef74d9
umluizlima <umluizlima@gmail.com>
2021-03-01 10:37:58 -0300

    Add Header component

---
+++ components/Header/index.js
@@ -0,0 +1,12 @@
+const Header = () => (
+  <header className="lg:text-center">
+    <h1 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+      Histórico de usados
+    </h1>
+    <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+      Acompanhe as variações de preço na tabela FIPE para veículos usados
+    </p>
+  </header>
+);
+
+export default Header;

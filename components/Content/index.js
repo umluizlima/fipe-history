90eba6cf7345c270c6256f1a3b9b0b49da6b0a48
umluizlima <umluizlima@gmail.com>
2021-03-21 20:24:25 -0300

    Update content

--- components/Content/index.js
+++ components/Content/index.js
@@ -1,11 +1,13 @@
+import ShareButton from '../ShareButton';
+
 const Content = () => (
   <div>
-    <div className="pb-10">
+    <div className="pb-5">
       <p className="text-xl leading-8 font-extrabold text-gray-900 sm:text-2xl">
-        Tome a melhor decisão
+        Como funciona?
       </p>
       <p className="mt-2 max-w-2xl text-lg text-gray-500">
-        Acompanhe a variação de preços para decidir se está na hora certa de comprar seu próximo veículo ou vender seu atual.
+        Esta página consulta diretamente o site da Tabela Fipe para o veículo escolhido, percorrendo as tabelas de referência disponíveis para montar um gráfico com seu preço médio mês a mês.
       </p>
     </div>
 
@@ -13,17 +15,17 @@ const Content = () => (
       <div className="flex">
         <div className="flex-shrink-0">
           <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
             </svg>
           </div>
         </div>
         <div className="ml-4">
           <dt className="text-lg leading-6 font-medium text-gray-900">
-            Compro ou espero?
+            Preços nos últimos 2 anos
           </dt>
           <dd className="mt-2 text-base text-gray-500">
-            O modelo que procura sofreu aumentos recentes? Pode valer a pena aguardar mais um pouco.
+            A consulta é limitada ao prazo máximo de 24 meses, onde veículos mais recentes apresentam menos resultados.
           </dd>
         </div>
       </div>
@@ -31,21 +33,28 @@ const Content = () => (
       <div className="flex">
         <div className="flex-shrink-0">
           <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
           </div>
         </div>
         <div className="ml-4">
           <dt className="text-lg leading-6 font-medium text-gray-900">
-            Vale vender?
+            Entenda a Tabela Fipe
           </dt>
           <dd className="mt-2 text-base text-gray-500">
-            Sua rotina mudou e não faz mais sentido manter um veículo próprio? Pode ser vantajoso colocar o dinheiro no bolso.
+            Visite o <a href="https://veiculos.fipe.org.br/" className="font-bold text-blue-500 hover:text-blue-700">site oficial</a> para saber mais detalhes sobre a metodologia aplicada na definição dos preços médios de veículos.
           </dd>
         </div>
       </div>
     </dl>
+
+    <div className="py-5">
+      <ShareButton
+        title="Histórico de usados"
+        description="Visualize as variações de preço na Tabela Fipe"
+      />
+    </div>
   </div>
 );
 

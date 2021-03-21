e437059f7227ca3723e2ade5a2f3c417149df33b
umluizlima <umluizlima@gmail.com>
2021-03-21 20:25:21 -0300

    Add Result component

---
+++ components/Result/index.js
@@ -0,0 +1,20 @@
+import FipePlot from '../FipePlot';
+
+const Result = ({ data }) => {
+  return (
+    <div className="pt-10">
+      <p className="text-xl leading-8 font-extrabold text-gray-900 sm:text-2xl">
+        Resultado
+      </p>
+      {!!data.length ? (
+        <FipePlot data={data}/>
+      ) : (
+        <p className="mt-2 max-w-2xl text-lg text-gray-500">
+          Preencha os campos para realizar uma consulta.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Result;

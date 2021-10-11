263f29bb65dddd4fa01debb36847b59d44bd273c
Luiz Lima <umluizlima@gmail.com>
2021-10-11 16:26:42 -0300

    Add meta robots tag

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Histórico de usados</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta name="robots" content="index,follow" />
       <meta name="description" content="Veja o histórico de preços de veículos usados segundo a tabela FIPE." />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <script data-goatcounter="https://usados-umluizlima.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>

a6878eb603296b5762531bf6dbc254631747efee
Luiz Lima <umluizlima@gmail.com>
2022-09-11 20:03:11 -0300

    Add donation link on Result

--- components/Result/index.js
+++ components/Result/index.js
@@ -33,6 +33,12 @@ const Result = ({ data }) => {
               <FipePlot data={data}/>
             </div>
           </li>
+          <li>
+            <h3 className="mt-2 text-lg leading-6 font-medium text-gray-900">Gostou?</h3>
+            <p className="mb-2 text-base text-gray-500">
+              Apoie este projeto com uma <a target="_blank" rel="noopener noreferrer" href="https://donate.stripe.com/6oE4hV8EjfY31O0cMM" className="font-bold text-blue-600 hover:text-blue-800">doação</a>.
+            </p>
+          </li>
         </ul>
       ) : (
         <p className="mt-2 max-w-2xl text-lg text-gray-500">

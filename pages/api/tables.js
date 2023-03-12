87860622734287d197369495a020486882a7627d
Luiz Lima <umluizlima@gmail.com>
2023-03-12 16:13:38 -0300

    Add tables endpoint

---
+++ pages/api/tables.js
@@ -0,0 +1,11 @@
+import { fetchTables } from "../../api/fipe"
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+
+  const tables = await fetchTables()
+
+  res.status(200).json(tables)
+}

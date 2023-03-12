af1935da49989b01ce5b3e382bbc84c5035588d9
Luiz Lima <umluizlima@gmail.com>
2023-03-12 17:23:28 -0300

    Update goatcounter integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Script from 'next/script';
+import { GCScript } from "next-goatcounter";
 import { DefaultSeo } from 'next-seo'
 
 import '../styles/globals.css';
@@ -12,7 +12,7 @@ const MyApp = ({ Component, pageProps }) => (
       <link rel="icon" href="/favicon.ico" />
       <meta name="robots" content="index,follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <Script data-goatcounter="https://usados-umluizlima.goatcounter.com/count" async src="//gc.zgo.at/count.js" />
+      <GCScript siteUrl={"https://usados-umluizlima.goatcounter.com/count"} />
     </Head>
     <Component {...pageProps} />
   </>

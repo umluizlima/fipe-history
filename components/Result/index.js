3bbfaa73ed9a0aaf6c821e9290a305f7e7b826f4
Luiz Lima <umluizlima@gmail.com>
2023-03-25 16:17:36 -0300

    Refactor page initialData to support multiple vehicles

--- components/Result/index.js
+++ components/Result/index.js
@@ -3,26 +3,45 @@ import ShareButton from '../ShareButton';
 import VehicleCard from '../VehicleCard';
 
 const Result = ({ data }) => {
-  const latestData = !!data.length && data.slice(-1)[0];
-  const vehicle = latestData && `${latestData['Marca']} ${latestData['Modelo']} ${`${latestData['AnoModelo']}`.replace('32000', 'Zero Km')}`;
+  const hasData = !!Object.keys(data).length;
+
+  const renderVehicleCards = (vehiclesData=[]) => (vehiclesData.map((vehicleData) => {
+    const latestData = !!vehicleData.length && vehicleData.slice(-1)[0];
+    const vehicle = latestData && `${latestData['Marca']} ${latestData['Modelo']} ${`${latestData['AnoModelo']}`.replace('32000', 'Zero Km')}`;
+    return (
+      <li>
+        <div className="mt-4">
+          <VehicleCard vehicle={vehicle} price={latestData['Valor']} />
+        </div>
+      </li>
+    );
+  }));
+
+  const renderShareButton = (vehiclesData=[]) => {
+    let message = "Veja o histórico de preços de veículos novos e usados";
+    if (vehiclesData.length === 1) {
+      const latestData = vehiclesData[0][0];
+      const vehicle = `${latestData['Marca']} ${latestData['Modelo']} ${`${latestData['AnoModelo']}`.replace('32000', 'Zero Km')}`;
+      message = `Veja o histórico de preços para ${vehicle}`;
+    }
+    return (
+      <li>
+        <div className="mt-2">
+          <ShareButton buttonText="Compartilhar consulta" title="Histórico de usados" description={message} />
+        </div>
+      </li>
+    );
+  }
 
   return (
     <div className="pt-10">
       <h2 id="resultado" className="text-2xl leading-8 font-extrabold text-gray-900">
         <a href="#resultado">Resultados</a>
       </h2>
-      {latestData ? (
+      {hasData ? (
         <ul>
-          <li>
-            <div className="mt-4">
-              <VehicleCard vehicle={vehicle} price={latestData['Valor']} />
-            </div>
-          </li>
-          <li>
-            <div className="mt-2">
-              <ShareButton buttonText="Compartilhar consulta" title="Histórico de usados" description={`Veja o histórico de preços para ${vehicle}`} />
-            </div>
-          </li>
+          {renderVehicleCards(Object.values(data))}
+          {renderShareButton(Object.values(data))}
           <li>
             {/* <h3 className="mt-2 text-lg leading-6 font-medium text-gray-900">Histórico</h3> */}
             <div className="mt-2 overflow-x-auto">
